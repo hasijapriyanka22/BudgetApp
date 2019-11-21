1a1f362508597890aca327f136e6531b726e1a0c
priyankahasija <priyanka.hasija@msystechnologies.com>
2019-11-21 17:23:53 +0530

    added controller

--- app.js
+++ app.js
@@ -1,262 +1,135 @@
-// // function abc(){
-// //     let a =10;
-// //     console.log(this);
-// //     innerFunction();
-// //     function innerFunction (){
-// //          console.log(this);
-// //          console.log('inner function');
-// //      }
-// //     this.one = function (){
-// //        // console.log(this);
+//Implementing module pattern using IIFEs (for encapsulation)
 
-// //         innerFunction();
-// //     }
-// //     Object.defineProperty( this, 'a', {
-// //         get: function(){
-// //             return a;
-// //         }
-// //     })
-// // }
-// // var obj1 = {
-// //     one : function(){
-// //         console.log('hi');
-// //     }
-// // }
-
-// // var obj = new abc();
-// // console.log(Object.getPrototypeOf(obj));
-// // console.log(obj.aB);
-// // obj.one();
-
-// // //obj.innerFunction();
-
-// // //console.log(obj);
-
-// // var BudgetController = (function(){
-// //     var x; 
-
-// //     function add(x)
-// //     {
-// //         return a+x;
-// //     }
-
-// //     var 
-// // })
-
-// // function sayHello(){
-// //     return 'hello';
-// // }
-
-
-
-// // function Circle(){
-// //     let name= 'anc';
-// //     this.name= name;
-// //     this.fxn = function(){
-// //         console.log('in function');
-// //     }
-// // }
-
-// // function greeting(message, name){
-// //     return function(){
-// //         //console.log(message() + name);
-// //     }
-// // }
-
-// // let fun = greeting(sayHello, 'priyanka')();
-// // console.log(fun);
-
-
-// // var person ={
-// //     name: 'priyanka',
-// //     fun: function(){
-// //         console.log(this);
-// //         function innerFunction(){
-// //             console.log(this);
-// //         }
-// //         innerFunction();
-// //     }
-// // }
-
-// // person.fun();
-
-// // function Person(name){
-// //     this.name = name;
-// // }
-// // var fxn = Array.prototype.map;
-
-// // Person.prototype.fxn = function (friends){
-// //     return friends.map( (el) => {
-// //             return `${this.name} my name is ${el}`;
-// //     });
-
-// // }
-
-// // var obj = new Person('priyanka').fxn(['kl', 'jk', 'j']);
-// // console.log(obj);
-
-// // var person = {
-// //     firstName : 'priyanka',
-// //     lastName : 'hasija'
-// // }
-
-// // const {firstName, lastName } = person;
-
-// // //Normally
-// // function sum(a,b,c,d){
-// //     return a+b+c+d;
-// // }
-// // var mySum = sum(1,3,4,2);
-// // //console.log(mySum);
-
-// // //ES5
-// // var arr= [1,2,3,4];
-// // function calcSum(a,b,c,d){
-// //     return a+b+c+d;
-// // }
-
-// // //var mySum1 = calcSum.apply(null, arr);
-// // //console.log(mySum1);
-
-// // //ES6
-// // //Spread Operator 
-// // var sum6 = calcSum(...arr);
-// // console.log(sum6);
-
-
-// // function fullAge(limit){
-// //     console.log(limit);
-// //     console.log(arguments);
-// //      var arr = Array.prototype.slice.call(arguments,1);
-// //      console.log(arr);
-// //      console.log(limit);
-// //    // console.log(args);
-// //     arr.forEach(element => {
-// //        //console.log(element) ;
-// //     });
-// // }
-
-// //fullAge(21, 1992,1995);
-// // var greeting = function(){
-// //     let counter = 0;
-// //     return {
-// //         inc : function(){
-// //         console.log(counter);
-// //         //console.log(this);
-// //         return counter++;
-// //     }
-// //   };
-// // };
-
-// function Person(name,lastName){
-//         this.firstName = name;
-//         this.lastName = lastName;
-// }
-
-
-// function Man(name,lastName,medals){
-//     Person.call(this,name,lastName);
-//     this.medals = medals;
-// }
-
-// Man.prototype = Object.create(Person.prototype);
-
-// var obj = new Man('priyanka', 'hasija', 8);
-
-
-// class A{
-//     constructor(name,lastName){
-//         this.name = name; 
-//         this.lastName = lastName;
-//     }
-//     static abc(){
-//         console.log('hi');
-//     }
-// }
-
-// class B extends A{
-//     // constructor(name,lastName, medals){
-//     //     super(name , lastName);
-//     //     this.medals = medals;
-//     // }
-
-// }
-
-// var obj1= new B('kl','jk', 90);
-// //obj1.abc();
-
-// class NullExtend extends null{
-
-// }
-// //var a=10;
-// console.log(a);
-
-// let BudgetController = (() => {
-//     var x =10;
-//     function add(a){
-//         console.log(x+a);
-//     }
-//     return {
-//         publicTest : function(a){
-//             console.log(x);
-//             add(a);
-//         }
-//     };
-// })();
-
-// let abc = (()=>{
-//     console.log('hi');
-// })();
-
-// function getRecipes(){
-//     setTimeout(()=>{
-//         const recipeIDs= [90,78,67];
-//         console.log(recipeIDs);
-//         setTimeout((id)=>{
-//             console.log(id);
-//             const details={title : 'pasta '};
-//             setTimeout((publisher)=>{
-
-//             })
-//         },1000,recipeIDs[0])
-//     },1500)
-// };
-//getRecipes();   
-
-var getIDs = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve([100, 101, 102]);
-    }, 1000)
-})
-
-var getRecipes = getID => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(getID);
-        }, 1000, getID);
-    });
-}
-
-// getIDs.then((id) => {
-//     console.log(id);
-//     return getRecipes(id[0]);
-// })
-//     .then((recpieId) => {
-//         console.log(recpieId);
-//     })
-//     .catch((result) => {
-//         console.log(result);
-//     });
-
-async function gettingIDs(){
-    const IDs = await getIDs;
-    console.log(IDs);
-    async function gettingRecipes(){
-        const recipe = await getRecipes(IDs[0]);
-        console.log(recipe);
+//BUDGET CONTROLLER
+var BudgetController = (function () {
+    var Expenses = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
     }
-    gettingRecipes();
-}
+
+    var Incomes = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0,
+        }
+    }
+
+    return {
+        addItem: function (type, des, val) {
+            var newItem, ID;
+
+            //Creating a new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type](data.allItems[type].length - 1).id + 1;
+            } else { ID = 0; }
+            if (type === 'inc') {
+                newItem = new Incomes(ID, des, val);
+            }
+            else {
+                newItem = new Expenses(ID, des, val);
+            }
+
+            //Adding it into our data structure
+            data.allItems[type].push(newItem);
+
+            //Return new element
+            return newItem;
+        },
+    }
+
+})();
+
+//UI CONTROLLER
+var UIController = (() => {
+
+    const DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    return {
+
+        //Getting input from UI
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,
+            }
+        },
+
+        //Getting ID, Classes of HTML elements
+        getDOMstings: function () {
+            return DOMstrings;
+        },
+
+        //Adding item to the UI
+        addListItem: function (obj, type) {
+
+            if (type === 'inc') {
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete" ><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div>'
+            }
+            else if (type === 'exp'){
+                html = '<div class="item clearfix" id="expense-0"><div class="item__description">Apartment rent</div><div class="right clearfix"><div class="item__value">- 900.00</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            }
+
+        }
+    };
+}) ();
+
+//GLOBAL APP CONTROLLER
+var Controller = ((BudgetCtrl, UICtrl) => {
+
+    function setUpEventListners() {
+
+        const DOM = UIController.getDOMstings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', () => ctrlAddItem());
+
+        document.addEventListener('keypress', (event) => {
+            if (event.keyCode == 13) {
+                ctrlAddItem();
+            }
+        });
+
+    }
+
+    function ctrlAddItem() {
+
+        var input, newItem;
+        //Get input fields
+        input = UICtrl.getInput();
+
+        //Add item to the budget controller
+        newItem = BudgetCtrl.addItem(input.type, input.description, input.value);
+
+        //Add new item to the UI 
+        UICtrl.addListItem(newItem, input.type);
+    }
+
+    return {
+        init: function () {
+            console.log('app is started');
+            setUpEventListners();
+        }
+    }
+
+})(BudgetController, UIController);
+
+Controller.init();
 
 
 
